00f3418d3e6bad55dd2605aa33133175eb7d70ac
Thomas Wendt <thoemy@gmx.net>
2013-10-21 19:14:25 +0200

    Add a config.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,29 @@
 LOCAL_PATH := $(call my-dir)
 
+include $(LOCAL_PATH)/config.mk
+
+L_CFLAGS := -g -Wall
+
+#
+# Process config
+#
+
+ifeq ($(FRAMEWORK_EXPECTS_ICS_AUDIO_BLOB),true)
+  L_CFLAGS += -DICS_AUDIO_BLOB
+else
+  L_CFLAGS += -DCONVERT_AUDIO_DEVICES_T
+endif
+
+ifneq ($(BUILD_AUDIO_POLICY_WRAPPER),true)
+  ifeq ($(HTC_ICS_AUDIO_BLOB),true)
+    L_CFLAGS += -DNO_HTC_POLICY_MANAGER
+  endif
+endif
+
+#
+# Audio Policy Wrapper
+#
+ifeq ($(BUILD_AUDIO_POLICY_WRAPPER), true)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
@@ -9,10 +33,9 @@ LOCAL_SRC_FILES := \
 
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libutils
-
 LOCAL_STATIC_LIBRARIES := libmedia_helper
 
-LOCAL_CFLAGS := -g -Wall -DCONVERT_AUDIO_DEVICES_T
+LOCAL_CFLAGS := $(L_CFLAGS)
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
@@ -20,8 +43,12 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 #include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
+endif
 
-
+#
+# Audio HW Wrapper
+#
+ifeq ($(BUILD_AUDIO_HW_WRAPPER), true)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
@@ -30,11 +57,9 @@ LOCAL_SRC_FILES := \
 
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libutils
-
 LOCAL_STATIC_LIBRARIES := libmedia_helper
 
-LOCAL_CFLAGS := -g -Wall -DCONVERT_AUDIO_DEVICES_T
-#LOCAL_CFLAGS += -DNO_HTC_POLICY_MANAGER
+LOCAL_CFLAGS := $(L_CFLAGS)
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
@@ -42,3 +67,4 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 #include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
+endif
---
+++ config.mk
@@ -0,0 +1,6 @@
+# Set this to true if ICS_AUDIO_BLOB is defined
+FRAMEWORK_EXPECTS_ICS_AUDIO_BLOB := true
+HTC_ICS_AUDIO_BLOB := true
+
+BUILD_AUDIO_POLICY_WRAPPER := true
+BUILD_AUDIO_HW_WRAPPER := true
